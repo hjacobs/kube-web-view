c49c791f90ab47cb0c057847f8b8e5a23785e29e
Henning Jacobs <henning@zalando.de>
2019-07-25 17:38:07 +0200

    test list view

---
+++ tests/e2e/test_list.py
@@ -0,0 +1,9 @@
+import requests
+
+
+def test_list_namespaced_resources(populated_cluster):
+    url = populated_cluster["url"].rstrip("/")
+    response = requests.get(f"{url}/clusters/default/namespaces/default/deployments")
+    response.raise_for_status()
+    assert "application=kube-web-view" in response.text
+    assert "kube-web-view-container" in response.text
