5bbe972ebca115b6fc4f14113052dc38658509cc
Henning Jacobs <henning@zalando.de>
2020-03-28 15:58:07 +0100

    order imports

--- kube_web/cluster_manager.py
+++ kube_web/cluster_manager.py
@@ -1,12 +1,11 @@
 import re
-
-from .resource_registry import ResourceRegistry
-from .selector import selector_matches
 from pathlib import Path
 from typing import Dict
 from typing import List
 
 from .cluster_discovery import OAuth2BearerTokenAuth
+from .resource_registry import ResourceRegistry
+from .selector import selector_matches
 
 INVALID_CLUSTER_NAME_CHAR_PATTERN = re.compile("[^a-zA-Z0-9:_.-]")
 
