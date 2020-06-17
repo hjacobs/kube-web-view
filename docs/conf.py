de0dc28e012b045813ed0bbe056573774c657b78
Henning Jacobs <henning@zalando.de>
2020-06-17 22:15:22 +0200

    v20.6.0

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@
 project = "Kubernetes Web View"
 copyright = "2019, Henning Jacobs"
 author = "Henning Jacobs"
-version = "20.5.0"
+version = "20.6.0"
 
 
 # -- General configuration ---------------------------------------------------
